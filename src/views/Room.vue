74872c4cc48c54d7f812d1abf91729cef6486a5c
EmptyRamenBox <alexis.martinezo92@gmail.com>
2020-04-19 04:37:32 -0500

    Finished App

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -13,13 +13,9 @@
       </v-list-item>
       <v-divider />
       <v-list nav>
-        <v-list-item
-          v-for="room in rooms"
-          :key="room.id"
-          :to="`/room/${room.id}`"
-        >
+        <v-list-item v-for="r in rooms" :key="r.id" :to="`/room/${r.id}`">
           <v-list-item-content>
-            <v-list-item-title>{{ room.name }}</v-list-item-title>
+            <v-list-item-title>{{ r.name }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <!-- -->
@@ -32,7 +28,7 @@
           {{ message.createdAt.toLocaleString() }}</v-card-subtitle
         >
         <v-card-text>
-          {{ message.text }}
+          {{ message.message }}
         </v-card-text>
       </v-card>
     </v-container>
@@ -52,38 +48,79 @@
 <script>
 // vuex-persistedstate always keeps user stored
 // yarn add vuex-persistedstate
+import { db } from "../plugins/firebase";
+import { mapGetters } from "vuex";
+
 export default {
   name: "Room",
   data() {
     return {
       newMessage: "",
-      room: {
-        id: 123,
-        name: "First",
-        createdAt: new Date(),
-      },
-      rooms: [
-        { id: 1, name: "Amazing Second" },
-        { id: 2, name: "Deja Vu I have been in this time before" },
-      ],
-      messages: [
-        {
-          id: 1,
-          text: "First message",
-          author: "Rodolfo Roman",
-          createdAt: new Date(),
-        },
-        {
-          id: 2,
-          text: "Second message",
-          author: "Rodolfo Roman",
-          createdAt: new Date(),
-        },
-      ],
+      room: {},
+      rooms: [],
+      messages: [],
     };
   },
+  computed: {
+    ...mapGetters({
+      user: "getUser",
+    }),
+  },
+  watch: {
+    // watcher
+    // will pay attention when something in particular happens
+    // IN THIS Case, the watcher will watch out when the route changes
+    "$route.params.id": async function() {
+      this.bind();
+      // to prevent list of chat rooms, the following line was added
+      await this.$bind(
+        "room",
+        db.collection("rooms").doc(this.$route.params.id)
+      );
+      await this.$bind(
+        "messages",
+        db
+          .collection("messages")
+          .where("roomId", "==", this.$route.params.id)
+          .orderBy("createdAt")
+      );
+    },
+  },
+  mounted() {
+    this.bind();
+  },
   methods: {
-    add() {},
+    async add() {
+      if (this.newMessage != "") {
+        await db.collection("messages").add({
+          roomId: this.$route.params.id,
+          author: this.user.displayName,
+          message: this.newMessage,
+          createdAt: new Date(),
+        });
+        this.newMessage = "";
+      }
+    },
+    async bind() {
+      // 3 different things to bind
+      // handles current room
+      await this.$bind(
+        "room",
+        db.collection("rooms").doc(this.$route.params.id)
+      );
+      // handles other rooms
+      // watcher, function that pays attention when something in particular happens
+      // in this case, we want to bind the room id when the route changes
+      await this.$bind("rooms", db.collection("rooms").orderBy("createdAt"));
+      // binding messages
+      await this.$bind(
+        "messages",
+        db
+          .collection("messages")
+          .where("roomId", "==", this.$route.params.id)
+          .orderBy("createdAt")
+      );
+    },
   },
 };
 </script>
